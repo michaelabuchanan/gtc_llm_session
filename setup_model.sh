64de34ced46e0d3221f2cad8ec7e91dfc0db7c70
Michaela Buchanan <michaela.buchanan@markiiisys.com>
2026-02-10 11:09:17 -0800

    Update setup_model.sh

--- setup_model.sh
+++ setup_model.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-mkdir ~/loras/Falcon3-7B-Base
-mv /home/lora/gtc_llm_session/* ~/loras/Falcon3-7B-Base
+export NIM_MODEL_NAME=hf://tiiuae/Falcon3-7B-Base
+export NIM_SERVED_MODEL_NAME=MedText-Falcon3-7B
+export NIM_CHAT_TEMPLATE=/home/gtc_llm_session.git/template_falcon.jinja
+export LOCAL_PEFT_DIRECTORY=/home/loras/
+
+mkdir /home/loras/Falcon3-7B-Base
+cp /home/gtc_llm_session.git/adapter_model.safetensors /home/loras/Falcon3-7B-Base
+cp /home/gtc_llm_session.git/adapter_config.json /home/loras/Falcon3-7B-Base
